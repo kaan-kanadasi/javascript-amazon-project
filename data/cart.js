6f727b527c40fe658eada114ff63f0b53f6e5a15
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-13 02:24:17 -0400

    commit

--- data/cart.js
+++ data/cart.js
@@ -1,16 +1,24 @@
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
 // export allows the variables to be used outside of cart.js
 // This approach helps avoid polluting the global namespace, which can lead to conflicts and harder-to-maintain code
-export const cart = [
-    {
+if(!cart) {
+    cart =
+    [{
         productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2
+        quantity: 2,
+        deliveryOptionId: '1'
     },
     {
         productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1
-    }
+        quantity: 1,
+        deliveryOptionId: '2'
+    }]
+};
 
-];
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -25,7 +33,22 @@ export function addToCart(productId) {
     } else {
         cart.push({
             productId: productId,
-            quantity: 1
+            quantity: 1,
+            deliveryOptionId: '1' // default value
         });
     }
-  }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+
+    saveToStorage();
+}
