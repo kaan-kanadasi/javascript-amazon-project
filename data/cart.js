77d96e8c73d71bb24837a39a74436f584eca55ad
kaan-kanadasi <kaankanadasi@gmail.com>
2024-10-12 21:37:40 -0400

    chnaged

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,16 @@
 // export allows the variables to be used outside of cart.js
 // This approach helps avoid polluting the global namespace, which can lead to conflicts and harder-to-maintain code
-export const cart = [];
+export const cart = [
+    {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    },
+    {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1
+    }
+
+];
 
 export function addToCart(productId) {
     let matchingItem;
